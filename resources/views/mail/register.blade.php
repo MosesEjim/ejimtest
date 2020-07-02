d09b1b05dc7b14dae3ef5792f5eef84ed4f8bea3
mosesejim <mosesejim@outlook.com>
2020-07-02 16:30:26 +0100

    added edit and delete to users module

---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <!-- <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection"> -->
+    <title></title>
+    <!--[if (mso 16)]>
+    <style type="text/css">
+    a {text-decoration: none;}
+    </style>
+    <![endif]-->
+    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+    <!--[if gte mso 9]>
+<xml>
+    <o:OfficeDocumentSettings>
+    <o:AllowPNG></o:AllowPNG>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+</head>
+
+<body style="background:white">
+    <div class="">
+        <!--[if gte mso 9]>
+			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+				<v:fill type="tile" color="#fafafa"></v:fill>
+			</v:background>
+		<![endif]-->
+       
+                        <table class="es-content" cellspacing="0" cellpadding="0" align="center">
+                            <tbody>
+                                <tr>
+                                    <td class="esd-stripe" style="background-color: #fafafa;" bgcolor="#fafafa" align="center">
+                                        <table class="es-content-body" style="background-color: #ffffff;" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="esd-structure es-p40t es-p20r es-p20l" style="background-color: transparent; background-position: left top;" bgcolor="transparent" align="left">
+                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="esd-container-frame" width="560" valign="top" align="center">
+                                                                        <table style="background-position: left top;" width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td class="esd-block-image es-p5t es-p5b" style="font-size:0" align="center"><a target="_blank"><img src="https://res.cloudinary.com/africonnet/image/upload/v1593695557/UNICEF.png" alt style="display: block;" width="175"></a></td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-p15t es-p15b" align="center">
+                                                                                        <h1 style="color: #333333; font-size: 20px;"><strong>EUM </strong></h1>
+                                                                                        <h1 style="color: #333333; font-size: 20px;"><strong>&nbsp;REGISTRATION</strong></h1>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-p40r es-p40l" align="left">
+                                                                                        <p style="text-align: center;">HI,&nbsp;{{$user->first_name}}&nbsp;{{$user->last_name}}</p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-p35r es-p40l" align="left">
+                                                                                        <p style="text-align: center;">You have been successfully registered on the Unicef EUM platform</p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-text es-p25t es-p40r es-p40l" align="center">
+                                                                                        <p>You login details are:</p>
+                                                                                        <tr>
+                                                                                            <td class="esd-block-text es-p25t es-p40r es-p40l" align="center"> <b>Username: &nbsp;</b>{{$user->email}}</td>
+                                                                                        </tr>
+                                                                                        <tr>
+                                                                                            <td class="esd-block-text es-p25t es-p40r es-p40l" align="center"> <b>Password: &nbsp;</b>{{$password}}</td>
+                                                                                        </tr>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td class="esd-block-button es-p40t es-p40b es-p10r es-p10l" align="center"><span class="es-button-border"><a href="" class="es-button" target="_blank">LOGIN</a></span></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                               
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
