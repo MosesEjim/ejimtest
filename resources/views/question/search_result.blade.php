96ee36e80a965ffbcf76332c6001954aa635ed79
Nanichang Katzing <nanipaul68@gmail.com>
2020-07-05 15:06:04 +0100

    m

--- resources/views/question/search_result.blade.php
+++ resources/views/question/search_result.blade.php
@@ -40,37 +40,50 @@
 
         @else
         <table class="table table-report sm:mt-2">
-          <thead>
-            <tr>
-              <th class="whitespace-no-wrap">S/N</th>
-              <th class="whitespace-no-wrap">NAME</th>
-              <th class="text-center whitespace-no-wrap">ACTIONS</th>
-            </tr>
-          </thead>
-          <tbody>
-            @php
-              $i = 1;
-            @endphp
-            @foreach($questions as $question)
-              <tr class="intro-x">
-                <td>
-                  <a href="" class="font-medium whitespace-no-wrap">{{ $i++ }} </a>                                  
-                </td>
-                <td>
-                  <a href="" class="font-medium">{{ $question->question_text }} </a>                                  
-                </td>
-                    
-                <td class="table-report__action w-56">
-                  <div class="flex justify-center items-center">
-                    <a class="flex items-center mr-3" href="{{ route('dashboard.uem.surveytype.question.edit', $question->id) }}"> <i data-feather="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
-                    <a class="flex items-center mr-3" href="{{ route('dashboard.uem.surveytype.question.show', $question->id) }}"> <i data-feather="eye" class="w-4 h-4 mr-1"></i> View </a>
-                    <a class="flex items-center text-theme-6" href="{{ route('dashboard.uem.surveytype.question.delete', $question->id) }}"> <i data-feather="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
-                  </div>
-                </td>
-              </tr>
-            @endforeach
-          </tbody>
-        </table>
+                    <thead>
+                      <tr>
+                        <th class="whitespace-no-wrap">S/N</th>
+                        <th class="whitespace-no-wrap">QUESTION</th>
+                        <th class="whitespace-no-wrap">PROGRAM</th>
+                        <th class="whitespace-no-wrap">SUB PROGRAM</th>
+                        <th class="whitespace-no-wrap">SURVEY TYPE</th>
+                        <th class="text-center whitespace-no-wrap">ACTIONS</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @php
+                        $i = 1;
+                      @endphp
+                      @foreach($questions as $question)
+                        <tr class="intro-x">
+                          <td>
+                              <a href="" class="font-medium whitespace-no-wrap">{{ $i++ }} </a>                                  
+                          </td>
+                          <td>
+                              <a href="" class="font-medium">{{ $question->question_text }} </a>                                  
+                          </td>
+                          
+                          <td>
+                              <a href="" class="font-medium">{{ $question->program_name }} </a>                                  
+                          </td>
+                          <td>
+                              <a href="" class="font-medium">{{ $question->sub_category_name }} </a>                                  
+                          </td>
+                          <td>
+                              <a href="" class="font-medium">{{ $question->survey_type_name }} </a>                                  
+                          </td>
+                              
+                          <td class="table-report__action w-56">
+                            <div class="flex justify-center items-center">
+                              <a class="flex items-center mr-3" href="{{ route('dashboard.uem.surveytype.question.edit', $question->id) }}"> <i data-feather="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
+                              <a class="flex items-center mr-3" href="{{ route('dashboard.uem.surveytype.question.show', $question->id) }}"> <i data-feather="eye" class="w-4 h-4 mr-1"></i> View </a>
+                              <a class="flex items-center text-theme-6" href="{{ route('dashboard.uem.surveytype.question.delete', $question->id) }}"> <i data-feather="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
+                            </div>
+                          </td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
         @endif
       </div>
     </div>
