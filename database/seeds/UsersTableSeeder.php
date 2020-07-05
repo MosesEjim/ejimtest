a3ca3e6da824167c4b40fd8f124509f132a87f40
Nanichang Katzing <nanipaul68@gmail.com>
2020-07-05 10:56:57 +0100

    m

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,67 +11,67 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // $superAdminCredentials = [
-        //     'first_name' => 'Super',
-        //     'last_name' => 'Admin',
-        //     'phone' => '1234567890',
-        //     'email' => 'superadmin@unicef.com',
-        //     'username' => 'superadmin',
-        //     'password' => 'secret',
-        //     'sex' => 'male',
-        //     'slug' => 'super-admin'
-        //   ];
+        $superAdminCredentials = [
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'phone' => '1234567890',
+            'email' => 'superadmin@unicef.com',
+            'username' => 'superadmin',
+            'password' => 'secret',
+            'sex' => 'male',
+            'slug' => 'super-admin'
+          ];
       
-        //   $superAdmin = Sentinel::registerAndActivate($superAdminCredentials, true);
-        //   $role = Sentinel::findRoleBySlug('superadmin');
-        //   $role->users()->attach($superAdmin);
+          $superAdmin = Sentinel::registerAndActivate($superAdminCredentials, true);
+          $role = Sentinel::findRoleBySlug('superadmin');
+          $role->users()->attach($superAdmin);
           
-        //   $adminCredentials = [
-        //     'first_name' => 'Admin',
-        //     'last_name' => 'Admin',
-        //     'phone' => '1234567890',
-        //     'email' => 'admin@unicef.com',
-        //     'username' => 'admin',
-        //     'password' => 'secret',
-        //     'sex' => 'male',
-        //     'slug' => 'admin'      
-        //   ];
+          $adminCredentials = [
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'phone' => '1234567890',
+            'email' => 'admin@unicef.com',
+            'username' => 'admin',
+            'password' => 'secret',
+            'sex' => 'male',
+            'slug' => 'admin'      
+          ];
       
-        //   $admin = Sentinel::registerAndActivate($adminCredentials, true);
-        //   $role = Sentinel::findRoleBySlug('admin');
-        //   $role->users()->attach($admin);
+          $admin = Sentinel::registerAndActivate($adminCredentials, true);
+          $role = Sentinel::findRoleBySlug('admin');
+          $role->users()->attach($admin);
           
-        //   $userCredentials = [
-        //     'first_name' => 'John',
-        //     'last_name' => 'Snow',
-        //     'phone' => '1234567890',
-        //     'email' => 'user@unicef.com',
-        //     'username' => 'guest',
-        //     'password' => 'secret',
-        //     'sex' => 'male',
-        //     'slug' => 'nani-snow'
-        //   ];
+          $userCredentials = [
+            'first_name' => 'John',
+            'last_name' => 'Snow',
+            'phone' => '1234567890',
+            'email' => 'user@unicef.com',
+            'username' => 'guest',
+            'password' => 'secret',
+            'sex' => 'male',
+            'slug' => 'nani-snow'
+          ];
           
-        //   $user = Sentinel::registerAndActivate($userCredentials, true);
-        //   $role = Sentinel::findRoleBySlug('user');
-        //   $role->users()->attach($user);
+          $user = Sentinel::registerAndActivate($userCredentials, true);
+          $role = Sentinel::findRoleBySlug('user');
+          $role->users()->attach($user);
 
           
-        //   //
-        //   $partnerCredentials = [
-        //     'first_name' => 'John',
-        //     'last_name' => 'Snow',
-        //     'phone' => '1234567890',
-        //     'email' => 'partner@unicef.com',
-        //     'username' => 'partner',
-        //     'password' => 'secret',
-        //     'sex' => 'male',
-        //     'slug' => 'partner-snow'
-        //   ];
+          //
+          $partnerCredentials = [
+            'first_name' => 'John',
+            'last_name' => 'Snow',
+            'phone' => '1234567890',
+            'email' => 'partner@unicef.com',
+            'username' => 'partner',
+            'password' => 'secret',
+            'sex' => 'male',
+            'slug' => 'partner-snow'
+          ];
           
-        //   $partner = Sentinel::registerAndActivate($partnerCredentials, true);
-        //   $role = Sentinel::findRoleBySlug('partner');
-        //   $role->users()->attach($partner);
+          $partner = Sentinel::registerAndActivate($partnerCredentials, true);
+          $role = Sentinel::findRoleBySlug('partner');
+          $role->users()->attach($partner);
 
           $dataCredentials = [
             'first_name' => 'Data',
